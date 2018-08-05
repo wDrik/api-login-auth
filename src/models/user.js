8ab5b2a74094015d0fbea3ddb934aa43f0862899
Iorgen Wildrik <w.drik@outlook.com>
2018-08-05 16:15:26 -0300

    Database and first model

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const UserScheme = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const User = mongoose.model('User', UserScheme);
+
+module.exports = User;
